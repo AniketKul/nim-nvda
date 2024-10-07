0e31755517af8740706503c2b1dd8b7d92571a1b
AniketKul <aniketmilindkulkarni@gmail.com>
2024-10-07 16:46:46 -0700

    Building RAG Document Q&A With Nvidia NIM And Langchain

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# nim-nvda
+# nim-nvda 
 
-Experimenting with Nvidia NIM
+## Building RAG Document Q&A With Nvidia NIM And Langchain
+
+- Ingest PDFs from `us_census` directory into loader
+- Load docs using loader.load()
+- Divide all documents into chunks
+- Convert all chunks to vector and store in the form of vector database
+
+## Demo
+
+![Screenshot1](demo_screenshots/Screen Shot 2024-10-07 at 16.33.10.png)
+![Screenshot2](demo_screenshots/Screen Shot 2024-10-07 at 16.35.56.png)
+![Screenshot3](demo_screenshots/Screen Shot 2024-10-07 at 16.41.21.png)
+![Screenshot4](demo_screenshots/Screen Shot 2024-10-07 at 16.41.48.png)
+
+
+
+
---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import os
+import time
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
+from langchain_community.document_loaders import PyPDFDirectoryLoader, WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+load_dotenv()
+
+# load Nvidia API Key
+os.environ['NVIDIA_API_KEY'] = os.getenv('NVIDIA_API_KEY')
+
+# Nvidia NIM inferencing
+llm = ChatNVIDIA(model='meta/llama-3.1-405b-instruct')
+
+def vector_embedding():
+    if 'vectors' not in st.session_state:
+        st.session_state.embeddings = NVIDIAEmbeddings()
+        # data ingestion
+        st.session_state.loader = PyPDFDirectoryLoader('./us_census') 
+        # document loading
+        st.session_state.docs = st.session_state.loader.load()
+        # chunk creation - works recursively
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=50)
+        # splitting - taking top 30 records
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:30])
+
+        print('Hi there!')
+
+        # vector openai embeddings i.e. our FAISS vector database
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, 
+                                                        st.session_state.embeddings)
+
+
+# main prompt logic
+st.title('Building RAG Document Q&A With Nvidia NIM And Langchain')
+
+prompt = ChatPromptTemplate.from_template(
+    '''
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Questions: {input}
+    '''
+)
+
+prompt1 = st.text_input('Enter your question from documents')
+
+if st.button('Start Document Embedding'):
+    vector_embedding()
+    st.write('FAISS VectorDB is ready using NVIDIAEmbeddings!')
+
+if prompt1:
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    start_time = time.process_time()
+    response = retrieval_chain.invoke({'input': prompt1})
+    print('Response Time: ', time.process_time() - start_time)
+    st.write(response['answer'])
+
+    # with streamlit expander
+    with st.expander('View Document Similarity Search'):
+        # find the relevant chunks
+        for i, doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write('-------')
